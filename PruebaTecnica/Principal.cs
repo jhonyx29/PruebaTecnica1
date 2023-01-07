c8d8b7a4363131dfdbb88f89ae9010104a9f280a
jhony <jhonyx03@gmail.com>
2023-01-07 10:47:02 -0500

    C

--- PruebaTecnica/Principal.cs
+++ PruebaTecnica/Principal.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.DataFormats;
 
 namespace PruebaTecnica
 {
@@ -19,14 +20,14 @@ namespace PruebaTecnica
 
         private void btnPrueba1_Click(object sender, EventArgs e)
         {
-            ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            Form1 frm = new Form1();
+            frm.Show();
         }
 
         private void btnPrueba2_Click(object sender, EventArgs e)
         {
-            ApplicationConfiguration.Initialize();
-            Application.Run(new Form2());
+            Form2 frm = new Form2();
+            frm.Show();
         }
     }
 }
